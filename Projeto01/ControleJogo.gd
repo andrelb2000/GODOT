825e70c4616b235d5c9b375ab639e3516d6fdebe
andrelb2000 <andre.luiz.braga@terra.com.br>
2022-02-03 11:39:22 -0300

    Primeira Versao

--- Projeto01/ControleJogo.gd
+++ Projeto01/ControleJogo.gd
@@ -2,15 +2,21 @@ extends Node2D
 
 
 # Declare member variables here. Examples:
-# var a = 2
+var x = 1
+var d = 0
 # var b = "text"
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	$Sprite.move_local_x(x)
+	d += 1
+	if d > 500:
+		$Sprite.move_local_x(-1 * d)
+		d = 0
