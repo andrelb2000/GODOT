9e656c3b5648b91e271b0bf5fccda0dbccb5820d
andrelb2000 <andre.luiz.braga@terra.com.br>
2022-02-03 11:55:41 -0300

    1 Vwrsao COndicional

--- Projeto01/ControleJogo.gd
+++ Projeto01/ControleJogo.gd
@@ -4,12 +4,15 @@ extends Node2D
 # Declare member variables here. Examples:
 var x = 1
 var d = 0
+var novoIcone 
+var iconeBase
 # var b = "text"
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	iconeBase = load("res://icon.png")
+	novoIcone = load("res://pop.jpg")
 
 
 
@@ -17,6 +20,11 @@ func _ready():
 func _process(delta):
 	$Sprite.move_local_x(x)
 	d += 1
+	if (d%7)==0:
+		$Sprite.texture = novoIcone
+	else:
+		$Sprite.texture = iconeBase
+		
 	if d > 500:
 		$Sprite.move_local_x(-1 * d)
 		d = 0
