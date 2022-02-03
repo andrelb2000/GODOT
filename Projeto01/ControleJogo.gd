1af87cd5dc14cfc464b0e4ce623c2c87d81b7edc
andrelb2000 <andre.luiz.braga@terra.com.br>
2022-02-03 12:13:53 -0300

    Nova Versao COndicional

--- Projeto01/ControleJogo.gd
+++ Projeto01/ControleJogo.gd
@@ -3,7 +3,8 @@ extends Node2D
 
 # Declare member variables here. Examples:
 var x = 1
-var d = 0
+var 
+var dx = 0
 var novoIcone = 0
 var iconeBase = 0
 # var b = "text"
@@ -19,12 +20,13 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	$Sprite.move_local_x(x)
-	d += 1
-	if ( ((d%7)>=0) and ((d%7)<7) )   :
+	dx += 1
+	if ( ((dx%100)>=0) and ((dy%100)<50) )   :
 		$Sprite.texture = novoIcone
 	else:
 		$Sprite.texture = iconeBase
 		
-	if d > 500:
-		$Sprite.move_local_x(-1 * d)
+	if dx > 500 or dy >500:
+		$Sprite.move_local_x(-1 * dx)
+		$Sprite.move_local_y(-1 * dy)
 		d = 0
