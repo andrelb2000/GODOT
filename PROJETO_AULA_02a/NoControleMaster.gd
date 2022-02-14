30ba03c93d9a2e6b6e67e7aeff0c113ef85b6fb9
andrelb2000 <andre.luiz.braga@terra.com.br>
2022-02-14 20:48:32 -0300

    Update NoControleMaster.gd

--- PROJETO_AULA_02a/NoControleMaster.gd
+++ PROJETO_AULA_02a/NoControleMaster.gd
@@ -3,18 +3,19 @@ extends Node2D
 const limiteEscala = 3
 const limiteX = 500
 const escalaInicial = 0.1
-var passo = 0.01
 var contador = escalaInicial
 var dx = 1
 var posicaoX = 0
 
-# Called when the node enters the scene tree for the first time.
+#DADOS ou INFORMACAO ARMAZENADA
+var passo = 0.01
+
+# PONTO DE ENTRADA DO PROGRAMA
 func _ready():
 	$TaxaCrescimento.text = String(passo)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	# CONTROLE - REGRAS DE NEGOCIO
 	contador += passo
 	if contador >= limiteEscala:
 		contador = escalaInicial
@@ -22,14 +23,13 @@ func _process(delta):
 	if posicaoX > limiteX  or posicaoX < 0:
 		dx = -dx
 
-	
+	# VISUAL - SAIDA de informações	
 	$Sprite.scale.x = contador
 	$Sprite.scale.y = contador	
 	$Sprite.move_local_x(dx)		
 	$Velocidade.text = String(dx)
-	
 
-
-
+# VISUAL - ENTRADA de informações
 func _on_Button_pressed():
+	#DADOS ou INFORMACAO ARMAZENADA
 	passo = float($TaxaCrescimento.text)
