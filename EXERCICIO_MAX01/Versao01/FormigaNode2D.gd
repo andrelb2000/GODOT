31339aa6ee5d64bd5ea6e392452f991ad985d737
andrelb2000 <andre.luiz.braga@terra.com.br>
2022-03-28 19:09:56 -0300

    Novos projetos

--- EXERCICIO_MAX01/Versao01/FormigaNode2D.gd
+++ EXERCICIO_MAX01/Versao01/FormigaNode2D.gd
@@ -4,7 +4,8 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+var antes = Vector2(0,0)
+var depois  = Vector2(0,0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,15 +13,22 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	update()
 
 
 func _on_ButtonMove_pressed():
+	antes = $AntManSprite.position 
 	$AntManSprite.move_local_x(10)
+	depois = $AntManSprite.position 
 
 
+func _draw():
+	draw_line(antes,depois,Color(10.0,0.0,0.0))
+	
+	
 func _on_ButtonSrink_pressed():
 	$AntManSprite.scale.x *=0.9
 	$AntManSprite.scale.y *=0.9
+	
 	
